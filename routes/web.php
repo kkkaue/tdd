6b29cd5ab1a9d9cb45a0305cee61ba4e14050c97
Kauê de Magalhães Brandão <kauedemagalhaes27@gmail.com>
2024-01-11 11:59:35 -0300

    Add RegisterController and update routes and tests

--- routes/web.php
+++ routes/web.php
@@ -1,24 +1,16 @@
 <?php
 
+use App\Http\Controllers\RegisterController;
 use App\Mail\Invitation;
 use App\Models\Invites;
 use Illuminate\Support\Facades\Mail;
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "web" middleware group. Make something great!
-|
-*/
-
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::post('register', RegisterController::class)->name('register');
 
 Route::post('invite', function () {
     Mail::to(request()->email)->send(new Invitation());
--- tests/Feature/RegisterTest.php
+++ tests/Feature/RegisterTest.php
@@ -2,9 +2,11 @@
 
 namespace Tests\Feature;
 
+use App\Mail\Invitation;
 use App\Models\User;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Mail;
 use Tests\TestCase;
 
@@ -18,35 +20,133 @@ class RegisterTest extends TestCase
         // Arrange
 
         // Act
+        $return = $this->post(route('register'), [
+            'name' => 'Kauê de Magalhães',
+            'email' => 'kauedemagalhaes27@gmail.com',
+            'email_confirmation' => 'kauedemagalhaes27@gmail.com',
+            'password' => 'senha Qualquer',
+        ]);
 
         // Assert
-    }
-    
-    /** @test */
-    public function it_should_be_able_to_invite_someone_to_the_platform()
-    {
-        // Arrange
-        Mail::fake();
+        $return->assertRedirect('dashboard');
+
+        $this->assertDatabaseHas('users', [
+            'name' => 'Kauê de Magalhães',
+            'email' => 'kauedemagalhaes27@gmail.com',
+        ]);
 
         /** @var User $user */
-        $user = User::factory()->create();
+        $user = User::whereEmail('kauedemagalhaes27@gmail.com')->firstOrFail();
 
-        $this->actingAs($user);
+        $this->assertTrue(
+            Hash::check('senha Qualquer', $user->password),
+            'Checking if password was saved and if is encrypted'
+        );
+
+        $this->assertAuthenticatedAs($user);
+    }
+
+    /** @test */
+    public function name_should_be_required()
+    {
+        // Arrange
 
         // Act
-
-        $this->post('invite', [
-            'email' => 'novo@email.com'
+        $return = $this->post(route('register'), [])
+            ->assertSessionHasErrors([
+                'name' => __('validation.required', ['attribute' => 'name']),
             ]);
+    }
 
-        // Assert
-        Mail::assertSent(Invitation::class, function ($mail) {
-            return $mail->hasTo('novo@email.com');
-        });
+    /** @test */
+    public function name_should_have_a_max_of_255_characters()
+    {
+        // Arrange
 
-        //criou um convite no banco de dados
-        $this->assertDatabaseHas('invitations', [
-            'email' => 'novo@email.com'
+        // Act
+        $this->post(route('register'), [
+            'name' => str_repeat('a', 256),
+        ])
+            ->assertSessionHasErrors([
+                'name' => __('validation.max.string', ['attribute' => 'name', 'max' => 255]),
+            ]);
+    }
+
+    /** @test */
+    public function email_should_be_required()
+    {
+        // Arrange
+
+        // Act
+        $this->post(route('register'), [])
+            ->assertSessionHasErrors([
+                'email' => __('validation.required', ['attribute' => 'email']),
+            ]);
+    }
+
+    /** @test */
+    public function email_should_be_a_valid_email()
+    {
+        // Arrange
+
+        // Act
+        $this->post(route('register'), [
+            'email' => 'invalid-email',
+        ])
+            ->assertSessionHasErrors([
+                'email' => __('validation.email', ['attribute' => 'email']),
+            ]);
+    }
+
+    /** @test */
+    public function email_should_be_unique()
+    {
+        // Arrange
+        User::factory()->create(['email' => 'test@email.com']);
+
+        // Act
+        $this->post(route('register'), ['email' => 'test@email.com'])
+            ->assertSessionHasErrors([
+                'email' => __('validation.unique', ['attribute' => 'email']),
+            ]);
+    }
+
+    /** @test */
+    public function email_should_be_confirmed()
+    {
+        // Arrange
+
+        // Act
+        $this->post(route('register'), [
+            'email' => 'test@email.com',
+            'email_confirmation' => 'invalid-email',
+        ])->assertSessionHasErrors([
+            'email' => __('validation.confirmed', ['attribute' => 'email']),
         ]);
+    }
+
+    /** @test */
+    public function password_should_be_required()
+    {
+        // Arrange
+
+        // Act
+        $this->post(route('register'), [])
+            ->assertSessionHasErrors([
+                'password' => __('validation.required', ['attribute' => 'password']),
+            ]);
+    }
+
+    /** @test */
+    public function password_should_have_at_least_1_uppercase()
+    {
+        // Arrange
+
+        // Act
+        $this->post(route('register'), [
+            'password' => 'password',
+        ])->assertSessionHasErrors([
+                'password' => 'The password field must contain at least one uppercase and one lowercase letter.',
+            ]);
     }
 }
