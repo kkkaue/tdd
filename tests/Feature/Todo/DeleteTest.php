be79ffa95ab81a09f8368302afda309e06c6d1b8
Kauê de Magalhães Brandão <kauedemagalhaes27@gmail.com>
2024-01-12 10:28:58 -0300

    Add authorization for deleting todos and create TodoPolicy

--- tests/Feature/Todo/DeleteTest.php
+++ tests/Feature/Todo/DeleteTest.php
@@ -16,7 +16,9 @@ class DeleteTest extends TestCase
         // Arrange
         /** @var User $user */
         $user = User::factory()->createOne();
-        $todo = Todo::factory()->createOne();
+        $todo = Todo::factory()->createOne([
+            'assigned_to_id' => $user->id,
+        ]);
 
         $this->actingAs($user);
         
@@ -27,6 +29,40 @@ class DeleteTest extends TestCase
         // Assert
         $this->assertDatabaseMissing('todos', [
             'id' => $todo->id,
+        ]);
+    }
+
+    /** @test */
+    public function only_the_assigned_user_should_be_able_to_delete_a_todo()
+    {
+        // Arrange
+        /** @var User $user */
+        $user1 = User::factory()->createOne();
+        $todoUser1 = Todo::factory()->createOne([
+            'assigned_to_id' => $user1->id,
+        ]);
+
+        /** @var User $user2 */
+        $user2 = User::factory()->createOne();
+        
+        // Act
+        $this->actingAs($user2);
+        $this->delete(route('todo.destroy', $todoUser1))
+            ->assertForbidden();
+
+        // Assert
+        $this->assertDatabaseHas('todos', [
+            'id' => $todoUser1->id,
+        ]);
+
+        // Act
+        $this->actingAs($user1);
+        $this->delete(route('todo.destroy', $todoUser1))
+            ->assertRedirect(route('todo.index'));
+
+        // Assert
+        $this->assertDatabaseMissing('todos', [
+            'id' => $todoUser1->id,
         ]);
     }
 }
