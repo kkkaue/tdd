bb6692f612e3362fe97f35a88c0c162b8715a8c5
Kauê de Magalhães Brandão <kauedemagalhaes27@gmail.com>
2024-01-12 10:18:43 -0300

    Refactor routes to use middleware for authentication and group related routes together

--- routes/web.php
+++ routes/web.php
@@ -14,14 +14,15 @@ Route::get('/', function () {
 
 Route::post('register', RegisterController::class)->name('register');
 
-Route::post('invite', function () {
-    Mail::to(request()->email)->send(new Invitation());
-    Invites::create([
-        'email' => request()->email
-    ]);
-}); 
-
-Route::get('todo', Todo\IndexController::class)->name('todo.index');
-Route::post('todo', Todo\CreateController::class)->name('todo.store');
-Route::put('todo/{todo}', Todo\UpdateController::class)->name('todo.update');
-Route::delete('todo/{todo}', Todo\DeleteController::class)->name('todo.destroy');
+Route::middleware('auth')->group(function () {
+    Route::post('invite', function () {
+        Mail::to(request()->email)->send(new Invitation());
+        Invites::create([
+            'email' => request()->email
+        ]);
+    }); 
+    Route::get('todo', Todo\IndexController::class)->name('todo.index');
+    Route::post('todo', Todo\CreateController::class)->name('todo.store');
+    Route::put('todo/{todo}', Todo\UpdateController::class)->name('todo.update');
+    Route::delete('todo/{todo}', Todo\DeleteController::class)->name('todo.destroy');
+});
