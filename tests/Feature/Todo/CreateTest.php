acca6918a5e17f553c16b94b3e1799304d440c91
Kauê de Magalhães Brandão <kauedemagalhaes27@gmail.com>
2024-01-12 09:55:50 -0300

    Add test for adding a file to a todo item

--- tests/Feature/Todo/CreateTest.php
+++ tests/Feature/Todo/CreateTest.php
@@ -5,12 +5,15 @@ namespace Tests\Feature\Todo;
 use App\Models\User;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Http\UploadedFile;
+use Illuminate\Support\Facades\Storage;
 use Tests\TestCase;
 
 class CreateTest extends TestCase
 {
     /** @test */
-    public function it_should_be_able_to_create_a_todo_item(){
+    public function it_should_be_able_to_create_a_todo_item()
+    {
         // Arrange
         /** @var User */
         $user = User::factory()->createOne();
@@ -33,5 +36,42 @@ class CreateTest extends TestCase
             'description' => 'Todo Item Description',
             'assigned_to_id' => $assignedTo->id,
         ]);
+    }
+
+    /** @test */
+    public function it_should_be_able_add_a_file_to_the_todo_item()
+    {
+        Storage::fake('s3');
+
+        // Arrange
+        //Create a user
+        /** @var User */
+        $user = User::factory()->createOne();
+
+        //Login with the user
+        $this->actingAs($user);
+
+        //Act
+        //Create a todo item
+        $request = $this->post(route('todo.store'), [
+            'title' => 'Todo Item',
+            'description' => 'Todo Item Description',
+            'assigned_to' => $user->id,
+            'file' =>  UploadedFile::fake()->image('file.jpg'), //Fake file
+        ]);
+
+        //Assert
+        //Check if file was uploaded
+        Storage::disk('s3')->assertExists('todo/file.jpg');
+
+        //Check if file path was saved in the database
+        $this->assertDatabaseHas('todos', [
+            'title' => 'Todo Item',
+            'description' => 'Todo Item Description',
+            'assigned_to_id' => $user->id,
+            'file_path' => 'todo/file.jpg',
+        ]);
+
+
     }
 }
